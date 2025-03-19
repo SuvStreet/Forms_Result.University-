58ac4808cc8ed6462ea54af3f9ffa83ac95a5aa3
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-19 21:01:11 +0300

    feat(textInput): Реализовать настраиваемый компонент input

---
+++ src/components/TextInput/TextInput.jsx
@@ -0,0 +1,51 @@
+import { Icon } from '../Icon/Icon'
+
+import './TextInput.css'
+
+export const TextInput = (dependencies) => {
+  const {
+    variant = 'default',
+    required,
+    label,
+    description,
+    placeholder,
+    error,
+    iconCode,
+    size = 'sm',
+    radius = 'sm',
+  } = dependencies
+
+  const hiddenError = () => {
+    if (error) {
+      return 'error'
+    } else {
+      return ''
+    }
+  }
+
+  return (
+    <div className="root_container" data-size={size}>
+      <label className="label">
+        {label}
+        {required && <span className="required">*</span>}
+      </label>
+      <small className="description">{description}</small>
+      <div
+        data-variant={variant}
+        className={`wrapper ${hiddenError()} radius-${radius}`}
+      >
+        {iconCode && (
+          <div className={`icon ${hiddenError()}`}>
+            <Icon iconCode={iconCode} />
+          </div>
+        )}
+        <input
+          type="text"
+          className={`input ${hiddenError()} `}
+          placeholder={placeholder}
+        />
+      </div>
+      {error && <div className="error errorMessage">Input error message</div>}
+    </div>
+  )
+}
