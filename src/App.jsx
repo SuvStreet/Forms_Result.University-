410025d40756355a2545b320fb1aa8da3d5e9403
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-19 21:01:11 +0300

    feat(textInput): Реализовать настраиваемый компонент input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { TextInput } from './components'
+
+import { faAt } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <div>
+        <TextInput
+          required
+          label="Input label"
+          description="Input description"
+          placeholder="Input placeholder"
+          // error="Input error message"
+          iconCode={faAt}
+          size="lg"
+        />
+      </div>
     </>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { TextInput } from './TextInput/TextInput.jsx'
+export { Icon } from './Icon/Icon.jsx'
